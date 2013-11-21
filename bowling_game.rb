0cb9c5c04eb56f3bbd5f8f5589e8bfcd275be8ba
MakerSquare Student <students@themakersquare.com>
2013-11-21 15:23:26 -0600

    Committed failing initialize test.

---
+++ bowling_game.rb
@@ -0,0 +1,26 @@
+
+
+class BowlingGame
+
+	def initialize
+		@rolls = []
+	end
+end
+
+
+
+# bowl has 10 frames
+
+# you get up to 2 tries each frame
+# if you get a strike on your first, you don't get a second try
+# if you don't get a strike, then you get a 2nd try
+
+
+# if in 2 tries, you don't knock them all down, your score is the sum of both attempts
+# 	if 2 tries, you knock them all down, you your score for the frame is 10 + the value of your next roll (1st try next frame)
+# 	If in 1 try you knock them all down, then your score is 10 + the sum of your next two rolls (even if not in the same frame)
+
+# On the last frame:
+# 	If you get a spare you get one bonus try
+# 	If you get a strike, you get two more bonus tries
+# You simply sum the tenth frame. 
