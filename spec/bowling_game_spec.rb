ef38ee899bf0a522a9406071bdb0d9cec9c2182a
MakerSquare Student <students@themakersquare.com>
2013-11-21 15:48:03 -0600

    Used 'let' function in our test to keep DRY.

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,9 +5,10 @@ require_relative '../bowling_game'
 
 
 describe BowlingGame do
+	let(:game) { BowlingGame.new}
+
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      game = BowlingGame.new
 
       expect(game.rolls).to eq([])
     end
@@ -15,14 +16,14 @@ describe BowlingGame do
 
 	describe '#score' do
 		it 'scores the game as 0 when only gutter balls are thrown' do
-			game = BowlingGame.new	
+	
 			20.times { game.roll(0)}
 
 			expect(game.score).to eq(0)
 		end 	
 
 		it 'scores the game as 20 when one pin is knocked down on each roll' do
-		  game = BowlingGame.new
+	
 		  20.times { game.roll(1) }
 
 		  expect(game.score).to eq(20)
