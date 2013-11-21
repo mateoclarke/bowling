203be20aba95684f2bde0831216a3c6fae788a9a
mateoclarke <code@mateoclarke.com>
2013-11-21 16:56:23 -0600

    Refactored for method decomposition with private methods for init_roll, strike?, score_strike, spare?, score_spare, score_normal

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,57 +1,84 @@
-
-
 class BowlingGame
-	attr_accessor :rolls
+  attr_accessor :rolls
 
-	def initialize
-		@rolls = []
+  def initialize
+    @rolls = []
+    @total_score  = 0
+    @current_roll = 0
+  end
+
+  # Record a roll in the game.
+  #
+  # pins - The Integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  # Returns the Integer score for this game. Expects to be run after all rolls
+  # for the game have been recorded.
+  def score
+    while @current_roll < @rolls.size
+      init_roll
+
+      if strike?
+        score_strike
+      elsif spare? 
+				score_spare      
+      else
+      	score_normal
+      end
+    end
+
+    return @total_score
+  end
+
+  private
+
+  # Initialize values for roll and next_roll.
+  #
+  # Returns nothing.
+  def init_roll
+    @roll      = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll + 1]
+  end
+
+  # Returns true if the current roll is a strike, false otherwise.
+  def strike?
+    @roll == 10
+  end
+
+
+  # Scores a strike frame, and adds it to the total score for the game.
+  #
+  # Returns nothing.
+  def score_strike
+    @total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+    @current_roll += 1
+  end
+
+  # Returns true is the current roll is a spare, false otherwise.
+	def spare?
+		@roll + @next_roll == 10
 	end
 
-	# Record a roll in the game.
-	# pins - The Integer number of pins knocked down in this roll.
+	# Scores a spare frame, and adds it to the total score.
+	# 
 	# Returns nothing.
-	def roll(pins)
-		@rolls << pins
+	def score_spare
+		@total_score += 10 + @rolls[@current_roll + 2]
+		@current_roll += 2
 	end
 
-	def score
-		total_score = 0
-		current_roll = 0
-
-		while current_roll < @rolls.size
-			roll      = @rolls[current_roll]
-			next_roll = @rolls[current_roll + 1]
-
-			if roll == 10
-				total_score += 10 + next_roll + @rolls[current_roll + 2]
-				current_roll += 1
-			elsif roll + next_roll == 10
-				total_score += 10 + @rolls[current_roll + 2]
-				current_roll += 2
-			else
-				total_score += roll + next_roll
-				current_roll += 2
-			end
-		end
-		
-		return total_score
+	# Scores a frame that sums to less that ten.
+	# 
+	# Returns nothing.
+	def score_normal
+		@total_score += @roll + @next_roll
+		@current_roll += 2
 	end
+
+
+
 end
-
-
-
-# bowl has 10 frames
-
-# you get up to 2 tries each frame
-# if you get a strike on your first, you don't get a second try
-# if you don't get a strike, then you get a 2nd try
-
-
-# if in 2 tries, you don't knock them all down, your score is the sum of both attempts
-# 	if 2 tries, you knock them all down, you your score for the frame is 10 + the value of your next roll (1st try next frame)
-# 	If in 1 try you knock them all down, then your score is 10 + the sum of your next two rolls (even if not in the same frame)
-
-# On the last frame:
-# 	If you get a spare you get one bonus try
-# 	If you get a strike, you get two more bonus tries
-# You simply sum the tenth frame. 
